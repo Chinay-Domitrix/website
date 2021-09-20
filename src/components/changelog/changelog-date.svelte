a65abfc88ba9148cb569c83d2d13cd96d899e611
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-09-20 16:00:38 +0000

    fix the spacing below the text decoration.

--- src/components/changelog/changelog-date.svelte
+++ src/components/changelog/changelog-date.svelte
@@ -6,9 +6,7 @@
 </script>
 
 <div class="w-full md:w-4/12">
-  <ChangelogLink {href}>
-    <span class="mb-xx-small h3 text-black">
-      {date}
-    </span>
+  <ChangelogLink class="mb-xx-small" {href}>
+    {date}
   </ChangelogLink>
 </div>
--- src/components/changelog/changelog-link.svelte
+++ src/components/changelog/changelog-link.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   export let href: string;
+  let clazz = "";
+  export { clazz as class };
 </script>
 
 <style type="text/postcss">
   a {
-    @apply no-underline;
+    @apply block no-underline;
   }
 
   a:hover,
@@ -12,8 +14,15 @@
     @apply underline;
     text-decoration-thickness: 2px;
   }
+
+  .h3 {
+    @apply text-h4 !important;
+  }
 </style>
 
-<a {href} class="text-black font-semibold  transition-all duration-200">
+<a
+  {href}
+  class="text-black h3 font-semibold transition-all duration-200 {clazz}"
+>
   <slot />
 </a>
